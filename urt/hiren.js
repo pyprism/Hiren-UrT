a5e6fbafbf9e1ca4ced33fe0ae8c0fc92f04da53
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-07-03 21:48:03 +0600

    Initial commit

---
+++ urt/hiren.js
@@ -0,0 +1,19 @@
+var io = require("socket.io")(http)
+function getConfig(){
+
+	//todo
+}
+
+
+function getServerDetails(host,port){
+    
+		//todo
+
+
+}
+
+  
+function getServerInfo(id, host, port){
+
+		//todo
+}
