9956ccc58ef067928c037c16bf12d56ec174f99b
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-07-05 20:53:39 +0600

    Initial Commit :wink:

--- server/hiren.js
+++ server/hiren.js
@@ -1,8 +1,32 @@
-/*
-*
-*
-*/
+var socket = require( "dgram" ).createSocket( "udp4" );
 
-function getStatus(address){
-	//TODO
+socket.on(
+	"message",
+	function ( message, requestInfo ) {
+		//response that we get from the udp server
+    	console.log("Message: " + message + " from " +requestInfo.address + ":" + requestInfo.port);
+	}
+);
+
+socket.on(
+	"error",
+	function ( error ) {
+	//error handling 
+	socket.close(); 
+	}
+);
+ 
+ 
+socket.on(
+	"listening",
+	function () {
+		var address = socket.address();	 
+		console.log( "socket listening " + address.address + ":" + address.port );
+	 
+	}
+);
+ 
+function getServerResponse(address){
+    //pass it udp server address 
+	socket.bind(address);
 }
